0c80ff05d886f707fd5bbe882741c1ba3724bfd0
EriveltonMGit <eriveltonprojetos.git@gmail.com>
2024-11-05 12:44:16 -0300

    chore:testando scroll horizontal

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <header className="hero__header">
-      <h1>John Doe</h1>
+      <h1>Erivelton Magalhães</h1>
       <h2>Frontend Developer</h2>
     </header>
   );
